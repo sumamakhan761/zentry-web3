cd89d9cbf8d87243793476b50bb608c70c8049a0
sumama <sumamakhan761>
2024-11-30 01:57:40 +0530

    final

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import About from "./components/About";
+import Hero from "./components/Hero";
+import NavBar from "./components/Navbar";
+import Features from "./components/Features";
+import Story from "./components/Story";
+import Contact from "./components/Contact";
+import Footer from "./components/Footer";
+import Gemini from "./components/google";
+import Font from "./components/font";
+
+function App() {
+  return (
+    <main className="relative min-h-screen w-screen overflow-x-hidden">
+      <NavBar />
+      <Hero />
+      <About />
+      <Features />
+      <Story />
+      <Gemini />
+      <Contact />
+      <Font />
+      <Footer />
+    </main>
+  );
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,52 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
+const colors = require("tailwindcss/colors");
+const {
+  default: flattenColorPalette,
+} = require("tailwindcss/lib/util/flattenColorPalette");
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    darkMode: "class",
+    extend: {
+      fontFamily: {
+        zentry: ["zentry", "sans-serif"],
+        general: ["general", "sans-serif"],
+        "circular-web": ["circular-web", "sans-serif"],
+        "robert-medium": ["robert-medium", "sans-serif"],
+        "robert-regular": ["robert-regular", "sans-serif"],
+      },
+      colors: {
+        blue: {
+          50: "#DFDFF0",
+          75: "#dfdff2",
+          100: "#F0F2FA",
+          200: "#010101",
+          300: "#4FB7DD",
+        },
+        violet: {
+          300: "#5724ff",
+        },
+        yellow: {
+          100: "#8e983f",
+          300: "#edff66",
+        },
+      },
+    },
+  },
+  plugins: [addVariablesForColors],
+};
+
+function addVariablesForColors({
+  addBase,
+  theme
+}) {
+  let allColors = flattenColorPalette(theme("colors"));
+  let newVars = Object.fromEntries(Object.entries(allColors).map(([key, val]) => [`--${key}`, val]));
+
+  addBase({
+    ":root": newVars,
+  });
+}
